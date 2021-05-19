fixed sign error in easy_c2g! more visualizations

cartpole.py
@@ -18,8 +18,9 @@ from pydrake.common import FindResourceOrThrow
 
 import matplotlib.pyplot as plt
 
-stiffness = 12
-base_wall_offset = 3.8
+stiffness = 5
+base_wall_offset = 13.8
+spring_length = 12.3
 
 def xyz_rpy_deg(xyz, rpy_deg):
     """Shorthand for defining a pose."""
@@ -36,12 +37,12 @@ def setup_walls(plant):
     right_joint = PrismaticJoint(
         "right_joint", plant.GetFrameByName("Wall", wall_right),
         plant.GetFrameByName("Wall", spring_wall_right), np.array([1, 0, 0]))
-    right_joint.set_default_translation(-2.3)
+    right_joint.set_default_translation(-spring_length)
     plant.AddJoint(right_joint)
     right_spring = LinearSpringDamper(
         plant.GetBodyByName("Wall", wall_right), np.array([0, 0, 0]),
         plant.GetBodyByName("Wall", spring_wall_right), np.array([0, 0, 0]),
-        2.3, stiffness, 0.0)
+        spring_length, stiffness, 0.0)
     plant.AddForceElement(right_spring)
 
     wall_left = Parser(plant).AddModelFromFile("wall.sdf", "wall_left")
@@ -53,12 +54,12 @@ def setup_walls(plant):
     left_joint = PrismaticJoint(
         "left_joint", plant.GetFrameByName("Wall", wall_left),
         plant.GetFrameByName("Wall", spring_wall_left), np.array([1, 0, 0]))
-    left_joint.set_default_translation(2.3)
+    left_joint.set_default_translation(spring_length)
     plant.AddJoint(left_joint)
     left_spring = LinearSpringDamper(
         plant.GetBodyByName("Wall", wall_left), np.array([0, 0, 0]),
         plant.GetBodyByName("Wall", spring_wall_left), np.array([0, 0, 0]),
-        2.3, stiffness, 0.0)
+        spring_length, stiffness, 0.0)
     plant.AddForceElement(left_spring)
 
 
@@ -67,9 +68,11 @@ V_cart_left = compute_lyapunov_function(deg_V=2, deg_L=2, mode="cart_left")
 V_cart_right = compute_lyapunov_function(deg_V=2, deg_L=2, mode="cart_right")
 V_pole_left = compute_lyapunov_function(deg_V=2, deg_L=2, mode="pole_left")
 V_pole_right = compute_lyapunov_function(deg_V=2, deg_L=2, mode="pole_right")
+Vs = [V_free, V_cart_left, V_pole_left, V_cart_right, V_pole_right]
+Vs = [V_free, V_free, V_free, V_free, V_free]
+cost_to_gos = [V.ToExpression() for V in Vs]
 
-function_fuser.fuse_functions(V_free, V_cart_left)
-sys.exit()
+#function_fuser.fuse_functions(V_free, V_cart_left)
 
 builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
@@ -85,7 +88,7 @@ visualizer = ConnectMeshcatVisualizer(
 visualizer.vis.delete()
 visualizer.set_planar_viewpoint(xmin=-2.5, xmax=2.5, ymin=-1.0, ymax=2.5)
 #controller = builder.AddSystem(CartpoleController(plant))
-controller = builder.AddSystem(LyapunovCartpoleController(plant, V_free.ToExpression()))
+controller = builder.AddSystem(LyapunovCartpoleController(plant, cost_to_gos))
 builder.Connect(plant.get_state_output_port(), controller.get_input_port(0))
 builder.Connect(controller.get_output_port(0),plant.get_actuation_input_port())
 diagram = builder.Build()
@@ -95,7 +98,7 @@ context = simulator.get_mutable_context()
 plant_context = plant.GetMyContextFromRoot(context)
 #plant.get_actuation_input_port(cartpole).FixValue(plant_context, np.array([0]))
 # Set the initial conditions
-context.SetContinuousState([1.0, np.pi - 0.5, -2.3, 2.3, 0.0, 0.0, 0, 0.00]) # x, theta, wall1, wall2, xdot, thetadot, wall1dot, wall2dot
+context.SetContinuousState([-1.0, np.pi - 0.5, -spring_length, spring_length, 0.0, 0.0, 0, 0.00]) # x, theta, wall1, wall2, xdot, thetadot, wall1dot, wall2dot
 context.SetTime(0.0)
 
 visualizer.start_recording()

lyapunov_controller.py
@@ -8,12 +8,13 @@ import utils
 
 m_c, m_p, g, l, k, x_01, x_02 = utils.get_system_parameters()
 variable_order = utils.variable_order
+modes = ["no_contact", "lc", "lp", "rc", "rp"]
 
 class LyapunovCartpoleController(LeafSystem):
-    def __init__(self, plant, cost_to_go):
+    def __init__(self, plant, cost_to_gos):
         LeafSystem.__init__(self)
         self.use_lqr = False
-        self.cost_to_go = cost_to_go
+        self.cost_to_gos = cost_to_gos
         self._plant = plant
         self._plant_context = plant.CreateDefaultContext()
         self._positions_port = self.DeclareVectorInputPort("configuration", BasicVector(8))
@@ -48,21 +49,22 @@ class LyapunovCartpoleController(LeafSystem):
         return variables, c2g_var_dict
 
     def get_lyapunov_control(self, cartpole_state):
+        mode = self.compute_mode(cartpole_state[0], cartpole_state[1])
         c2g_state = self.cost_to_go_state(cartpole_state)
-        variables, c2g_var_dict = self.c2g_dict_variables(c2g_state, self.cost_to_go)
+        variables, c2g_var_dict = self.c2g_dict_variables(c2g_state, self.cost_to_gos[mode])
         #prog = MathematicalProgram()
         #u = prog.NewContinuousVariables(1, "u")[0]
         u = Variable("u")
         f = utils.compute_dynamics(*c2g_state, u)
-        Vdot = self.cost_to_go.Jacobian(variables).dot(f)
+        Vdot = self.cost_to_gos[mode].Jacobian(variables).dot(f)
         Vdot_at_state = Vdot.Substitute(c2g_var_dict)
-        V_at_state = self.cost_to_go.Substitute(c2g_var_dict)
+        #V_at_state = self.cost_to_gos[mode].Substitute(c2g_var_dict)
         output_1 = Vdot_at_state.Substitute({u: 30})
         output_2 = Vdot_at_state.Substitute({u: -30})
         if float(output_1.to_string()) < float(output_2.to_string()):
-            return 50
+            return 30
         else:
-            return -50
+            return -30
         #return control
         #print("c2g = ", V_at_state)
         #prog.AddCost(Vdot_at_state)
@@ -74,11 +76,22 @@ class LyapunovCartpoleController(LeafSystem):
         #control = result.GetSolution(u)
         #print("picking: ", control)
 
+    def compute_mode(self, x, theta):
+        if x < x_01:
+            return 1
+        elif x + l*np.sin(theta) < x_01:
+            return 2
+        elif x > x_02:
+            return 3
+        elif x + l*np.sin(theta) > x_02:
+            return 4
+        else:
+            return 0
+
     def CalcOutput(self, context, output):
         state = self._positions_port.Eval(context)
         cartpole_state = np.array([state[0], state[1], state[4], state[5]])
-        if cartpole_state[0] < -21.5:
-            print("bump left cart")
+
         #lqr output
         lqr_state = np.copy(cartpole_state)
         lqr_state[1] = lqr_state[1] - np.pi
@@ -88,5 +101,5 @@ class LyapunovCartpoleController(LeafSystem):
             tau = tau_lqr
         else:
             tau = np.array([self.get_lyapunov_control(cartpole_state)])
-        tau = np.clip(tau, -15.0, 15.0)
+        tau = np.clip(tau, -25.0, 25.0)
         output.SetFromVector(tau)

visualize_lyapunov.py
@@ -45,6 +45,8 @@ class MockLyapunovIO():
 
     def get_lyapunov_output(self, cartpole_state):
         c2g_state = self.cost_to_go_state(cartpole_state)
+        if abs(cartpole_state[1] - np.pi) < 0.1:
+            print("c2g state: ", c2g_state)
         variables, c2g_var_dict = self.c2g_dict_variables(c2g_state, self.cost_to_go)
         prog = MathematicalProgram()
         u = prog.NewContinuousVariables(1, "u")[0]
@@ -71,6 +73,7 @@ class MockLyapunovIO():
         return float(V_at_state.to_string()), control
 
     def compute_V_outputs(self):
+        #x = np.linspace(0, 2*np.pi)
         x = np.linspace(-10, 10)
         V_outputs = list()
         control_outputs = list()
@@ -84,13 +87,20 @@ class MockLyapunovIO():
 
 if __name__ == "__main__":
 
-    V_poly = compute_lyapunov_function(deg_V=4, deg_L=4)
+    V_poly = compute_lyapunov_function(deg_V=2, deg_L=2)
     #V_poly = V_poly.RemoveTermsWithSmallCoefficients(1e-6)
     #print(V_print)
     V = V_poly.ToExpression()
     lyapunov_mocker = MockLyapunovIO(V)
     states,V_outputs, control_outputs = lyapunov_mocker.compute_V_outputs()
     plt.plot(states, control_outputs)
+    plt.xlabel("x position")
+    plt.ylabel("control")
+    plt.title("Control policy")
     plt.show()
+    plt.xlabel("x position")
+    plt.ylabel("Value")
+    plt.title("Value function with respect to varying position")
+
     plt.plot(states, V_outputs)
     plt.show()